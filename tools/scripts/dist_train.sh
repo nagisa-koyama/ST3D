8a304f8c7ac2adbb9ba4f1ac373a4a747674b88d
Nagisa Koyama <nagisa.koyama@gmail.com>
2022-11-17 00:12:39 +0900

    Add config for dist_train.sh

--- tools/scripts/dist_train.sh
+++ tools/scripts/dist_train.sh
@@ -3,6 +3,12 @@
 set -x
 NGPUS=$1
 PY_ARGS=${@:2}
+MASTER_ADDR=4000
+MASTER_PORT=4000
+WORLD_SIZE=2
+RANK=0
+LOCAL_RANK=0
+NODE_RANK=0
 
 python -m torch.distributed.launch --nproc_per_node=${NGPUS} train.py --launcher pytorch ${PY_ARGS}
 
