e95042dc0d166cae7379ab907f3c306c46013aca
Nagisa Koyama <nagisa.koyama@gmail.com>
2023-07-04 00:35:15 +0900

    Comment out lines to avoid pth upload

--- tools/train.py
+++ tools/train.py
@@ -281,9 +281,9 @@ def main():
 
     # Exclude pth files from wandb upload.
     # TODO: Make this work.
-    pth_list = glob.glob(str(cfg.ROOT_DIR / '**/*.pth'), recursive=True)
-    pth_list_str = ','.join(pth_list)
-    os.environ['WANDB_IGNORE_GLOBS'] = pth_list_str
+    # pth_list = glob.glob(str(cfg.ROOT_DIR / '**/*.pth'), recursive=True)
+    # pth_list_str = ','.join(pth_list)
+    # os.environ['WANDB_IGNORE_GLOBS'] = pth_list_str
 
     # Copy wandb folder to storage here in case following evaluation fails.
     copy_cmd = "cp -Lr wandb /storage/"
