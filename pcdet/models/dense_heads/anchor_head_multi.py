9b50f4237f992ae665536b156e5ace34fabba254
Nagisa Koyama <nagisa.koyama@gmail.com>
2024-10-14 17:52:36 +0900

    Fix config of dann loss weight

--- pcdet/models/dense_heads/anchor_head_multi.py
+++ pcdet/models/dense_heads/anchor_head_multi.py
@@ -416,16 +416,18 @@ class AnchorHeadMulti(AnchorHeadTemplate):
     def get_domain_adversarial_loss(self):
         loss_weights = self.model_cfg.LOSS_CONFIG.LOSS_WEIGHTS
         # Returns None if dann_weight or domain_label is not set.
-        if 'dann_weight' not in loss_weights or 'domain_label' not in self.forward_ret_dict:
+        if 'dann_weight' not in loss_weights or 'domain_label' not in self.forward_ret_dict.keys():
             return None, {}
 
         dann_loss_weight = loss_weights['dann_weight']
         domain_preds = self.forward_ret_dict['domain_preds']
         domain_label = self.forward_ret_dict['domain_label']
+        print("domain_label: ", domain_label)
         batch_size = int(domain_preds[0].shape[0])
 
         loss = nn.BCEWithLogitsLoss()(domain_preds, Variable(
             torch.FloatTensor(domain_preds.data.size()).fill_(domain_label)).cuda())
         loss = loss.sum() / batch_size * dann_loss_weight
+        tb_dict = {}
         tb_dict['rpn_loss_dann'] = loss.item()
         return loss, tb_dict
