70cc98d4180e348a836b45cf8a11039b1c2fc8a0
Nagisa Koyama <nagisa.koyama@gmail.com>
2024-12-02 23:59:17 +0900

    Replace DANN head to simpler two 1x1 conv head

--- pcdet/models/dense_heads/anchor_head_multi.py
+++ pcdet/models/dense_heads/anchor_head_multi.py
@@ -3,7 +3,7 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 
-from ...ops.dann.functions import DomainDiscriminator, ReverseLayerF
+from ...ops.dann.functions import DomainDiscriminator, ReverseLayerF, DAImgHead
 from ..backbones_2d import BaseBEVBackbone
 from .anchor_head_template import AnchorHeadTemplate
 
@@ -190,7 +190,8 @@ class AnchorHeadMulti(AnchorHeadTemplate):
             shared_conv_num_filter = input_channels
         self.rpn_heads = None
         self.make_multihead(shared_conv_num_filter)
-        self.domain_discriminator = DomainDiscriminator(in_channels=shared_conv_num_filter)
+        # self.domain_discriminator = DomainDiscriminator(in_channels=shared_conv_num_filter)
+        self.domain_discriminator = DAImgHead(in_channels=shared_conv_num_filter)
 
     def make_multihead(self, input_channels):
         rpn_head_cfgs = self.model_cfg.RPN_HEAD_CFGS
--- pcdet/ops/dann/functions.py
+++ pcdet/ops/dann/functions.py
@@ -12,14 +12,15 @@ class ReverseLayerF(Function):
 
     @staticmethod
     def backward(ctx, grad_output):
+        # output = grad_output * ctx.alpha
         output = grad_output.neg() * ctx.alpha
 
         return output, None
 
 
-# Refers to file:///Users/nagisa/Downloads/1910.11319v1.pdf
+# From https://github.com/nagisa-koyama/DA_detection/blob/master/lib/nets/discriminator_img.py
 class DomainDiscriminator(nn.Module):
-    def __init__(self, in_channels, out_channels = 1, inter_channels=64):
+    def __init__(self, in_channels, out_channels=1, inter_channels=64):
         super(DomainDiscriminator, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, inter_channels, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(inter_channels, inter_channels, kernel_size=3, padding=1)
@@ -37,4 +38,37 @@ class DomainDiscriminator(nn.Module):
         x = self.leaky_relu(x)
         x = self.classifier(x)
 
+        return x
+
+
+# From https://github.com/nagisa-koyama/Domain-Adaptive-Faster-RCNN-PyTorch/blob/master/maskrcnn_benchmark/modeling/da_heads/da_heads.py
+class DAImgHead(nn.Module):
+    """
+    Adds a simple Image-level Domain Classifier head
+    """
+
+    def __init__(self, in_channels):
+        """
+        Arguments:
+            in_channels (int): number of channels of the input feature
+            USE_FPN (boolean): whether FPN feature extractor is used
+        """
+        super(DAImgHead, self).__init__()
+        self.mid_channels = 64
+        self.conv1_da = nn.Conv2d(in_channels, self.mid_channels, kernel_size=1, stride=1)
+        self.conv2_da = nn.Conv2d(self.mid_channels, 1, kernel_size=1, stride=1)
+        self.relu = nn.ReLU()
+
+        for l in [self.conv1_da, self.conv2_da]:
+            nn.init.normal_(l.weight, std=0.001)
+            nn.init.constant_(l.bias, 0)
+
+    def forward(self, x):
+        # img_features = []
+        # for feature in x:
+        #     t = self.relu(self.conv1_da(feature))
+        #     img_features.append(self.conv2_da(t))
+        x = self.conv1_da(x)
+        x = self.relu(x)
+        x = self.conv2_da(x)
         return x
