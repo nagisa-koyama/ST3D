7ce28d7654caee695029862852d3ea8a7c976a75
Nagisa Koyama <nagisa.koyama@gmail.com>
2025-01-30 00:03:45 +0900

    Comment out heuristic on downsampling

--- pcdet/datasets/processor/data_processor.py
+++ pcdet/datasets/processor/data_processor.py
@@ -150,18 +150,19 @@ class DataProcessor(object):
 
         points = data_dict['points']
         if num_points < len(points):
-            pts_depth = np.linalg.norm(points[:, 0:3], axis=1)
-            pts_near_flag = pts_depth < 40.0
-            far_idxs_choice = np.where(pts_near_flag == 0)[0]
-            near_idxs = np.where(pts_near_flag == 1)[0]
-            choice = []
-            if num_points > len(far_idxs_choice):
-                near_idxs_choice = np.random.choice(near_idxs, num_points - len(far_idxs_choice), replace=False)
-                choice = np.concatenate((near_idxs_choice, far_idxs_choice), axis=0) \
-                    if len(far_idxs_choice) > 0 else near_idxs_choice
-            else:
-                choice = np.arange(0, len(points), dtype=np.int32)
-                choice = np.random.choice(choice, num_points, replace=False)
+            # TODO(nagisa): revisit this if the performance is not good enough.
+            # pts_depth = np.linalg.norm(points[:, 0:3], axis=1)
+            # pts_near_flag = pts_depth < 40.0
+            # far_idxs_choice = np.where(pts_near_flag == 0)[0]
+            # near_idxs = np.where(pts_near_flag == 1)[0]
+            # choice = []
+            # if num_points > len(far_idxs_choice):
+            #     near_idxs_choice = np.random.choice(near_idxs, num_points - len(far_idxs_choice), replace=False)
+            #     choice = np.concatenate((near_idxs_choice, far_idxs_choice), axis=0) \
+            #         if len(far_idxs_choice) > 0 else near_idxs_choice
+            # else:
+            choice = np.arange(0, len(points), dtype=np.int32)
+            choice = np.random.choice(choice, num_points, replace=False)
             np.random.shuffle(choice)
         else:
             choice = np.arange(0, len(points), dtype=np.int32)
