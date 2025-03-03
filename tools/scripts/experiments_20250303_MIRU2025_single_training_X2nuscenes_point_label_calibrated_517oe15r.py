f9dc2fff7f7b70d91409710c186b70d2c156fe9c
Nagisa Koyama <nagisa.koyama@gmail.com>
2025-03-04 00:16:25 +0900

    Make point label calibrated configs load preprocess_XX_wo_aug.yaml

---
+++ tools/scripts/experiments_20250303_MIRU2025_single_training_X2nuscenes_point_label_calibrated_517oe15r.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+
+common_suffix = "MIRU2025_point_label_calibrated_517oe15r"
+common_platform = "-platform offscreen"
+common_args = "--batch_size 20"
+
+dict_cfg_lyft2nuscenes = {
+    "name": f"train_lyft2nuscenes_{common_suffix}",
+    "script": 'train.py',
+    "cfg_file": "cfgs/da-MIRU2025/second_old_anchor_basebev_multi_lyft2nuscenes_car_ped_point_label_calibrated.yaml",
+    "teacher_ckpt": "",
+    "ckpt_dir": "",
+    "args": common_args,
+    "platform": common_platform,
+}
+
+dict_cfg_kitti2nuscenes = {
+    "name": f"train_kitti2nuscenes_{common_suffix}",
+    "script": 'train.py',
+    "cfg_file": "cfgs/da-MIRU2025/second_old_anchor_basebev_multi_kitti2nuscenes_car_ped_point_label_calibrated.yaml",
+    "teacher_ckpt": "",
+    "ckpt_dir": "",
+    "args": common_args,
+    "platform": common_platform,
+}
+
+dict_cfgs = [dict_cfg_lyft2nuscenes, dict_cfg_kitti2nuscenes]
+
+for dict_cfg in dict_cfgs:
+    cmd = "python"
+    if dict_cfg["script"]:
+        cmd += " " + dict_cfg["script"]
+    if dict_cfg["cfg_file"]:
+        cmd += " --cfg_file " + dict_cfg["cfg_file"]
+    if dict_cfg["name"]:
+        cmd += " --run_name " + dict_cfg["name"]
+    if dict_cfg["args"]:
+        cmd += " " + dict_cfg["args"]
+    if dict_cfg["platform"]:
+        cmd += " " + dict_cfg["platform"]
+    if dict_cfg["teacher_ckpt"]:
+        cmd += " --pretrained_model_teacher " + dict_cfg["teacher_ckpt"]
+    if dict_cfg["ckpt_dir"]:
+        cmd += " --ckpt_dir " + dict_cfg["ckpt_dir"] + " --eval_all"
+
+    print(cmd)
+    subprocess.call(cmd.split())
