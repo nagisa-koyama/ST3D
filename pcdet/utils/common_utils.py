a2bfbdf6ca9168b9532f8dcd229d4947ded95153
Nagisa Koyama <nagisa.koyama@gmail.com>
2025-08-14 11:24:44 +0900

    Update next() to __next__() to adapt to python 3.10

--- pcdet/utils/common_utils.py
+++ pcdet/utils/common_utils.py
@@ -220,12 +220,12 @@ class DataReader(object):
 
     def read_data(self):
         try:
-            return self.dataloader_iter.next()
+            return next(self.dataloader_iter.next)
         except:
             if self.sampler is not None:
                 self.sampler.set_epoch(self.cur_epoch)
             self.construct_iter()
-            return self.dataloader_iter.next()
+            return next(self.dataloader_iter.next)
 
 
 class AverageMeter(object):
