abb3b18272f2146eee5eaae5cb7cdfd5cabc8fee
Nagisa Koyama <nagisa.koyama@gmail.com>
2026-03-01 08:59:19 +0900

    Integrage CenterPoint

--- pcdet/models/dense_heads/__init__.py
+++ pcdet/models/dense_heads/__init__.py
@@ -1,6 +1,7 @@
 from .anchor_head_multi import AnchorHeadMulti
 from .anchor_head_single import AnchorHeadSingle
 from .anchor_head_template import AnchorHeadTemplate
+from .center_head import CenterHead
 from .point_head_box import PointHeadBox
 from .point_head_simple import PointHeadSimple
 from .point_intra_part_head import PointIntraPartOffsetHead
@@ -12,4 +13,5 @@ __all__ = {
     'PointHeadSimple': PointHeadSimple,
     'PointHeadBox': PointHeadBox,
     'AnchorHeadMulti': AnchorHeadMulti,
+    'CenterHead': CenterHead,
 }
